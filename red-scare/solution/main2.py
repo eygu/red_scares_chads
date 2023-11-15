a8636502c4d38115c5550c852a253e542e68dbea
Eyzzi <eythor@vista.is>
2023-11-15 14:53:02 +0100

    fixed directory hardcode doodad

--- red-scare/solution/main2.py
+++ red-scare/solution/main2.py
@@ -1,4 +1,5 @@
 from read_files import read_all_files, read_graph
+import os 
 
 def case_none(s, t, graph, word_to_idx, is_word):
     if is_word:
@@ -11,11 +12,12 @@ def case_none(s, t, graph, word_to_idx, is_word):
     return graph.dijkstra(s, t)
 
 def main():
-    # path = r"/Users/williamforbrigd/dev/itu/01/algorithmdesign/red_scares_chads/red-scare/instance-generators/handmade"
-    path = r"/Users/williamforbrigd/dev/itu/01/algorithmdesign/red_scares_chads/red-scare/data"
+    # path = r"/Users/williamforbrigd/dev/itu/01/algorithmdesign/red_scares_chads/red-scare/data"
     # file = "rusty-1-17.txt"
+
+    dataDirectory =  os.path.dirname(os.path.realpath(__file__)) + '/../data'
     file = "grid-5-0.txt"
-    path = rf"{path}/{file}"
+    path = rf"{dataDirectory}/{file}"
     n, m, r, s, t, graph, word_to_idx, is_word = read_graph(path)
     res = case_none(s, t, graph, word_to_idx, is_word)
     print(f"file: {file}, dijkstra result: {res}")
