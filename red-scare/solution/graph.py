d708f923bf742a9c11069751bc10c1f28a86d270
JoakimAustvold <joakim.austvold@gmail.com>
2023-11-25 14:06:18 +0100

    added acyclical detection

--- red-scare/solution/graph.py
+++ red-scare/solution/graph.py
@@ -1,5 +1,6 @@
 from collections import defaultdict, deque
 from heapq import heappop as pop, heappush as push
+from G import G
 
 class Graph:
     def __init__(self, size, G, nodes):
@@ -90,10 +91,81 @@ class Graph:
 
         return False
 
+    def bfs_many(self, start, finish):
+        if self.isCyclic():
+            return -1
+        #TODO: actually make the code
+        return
     
     def __repr__(self):
         return f"Graph({self.G})"
-
-
-
     
+    def isCyclicUtil(self, v, visited, recStack, path):
+        visited[v] = True
+        recStack[v] = True
+        path.append(v)
+
+        for neighbour in self.G.G[v].keys():
+            if visited[neighbour] == False:
+                if self.isCyclicUtil(neighbour, visited, recStack, path) == True:
+                    return True
+            elif recStack[neighbour] == True:
+                # Cycle detected, print the path for reference
+                print("Cycle detected:", path + [neighbour])
+                return True
+
+        recStack[v] = False
+        path.pop()
+        return False
+
+    # Returns true if graph is cyclic else false
+    def isCyclic(self):
+        visited = [False] * len(self.nodes)
+        recStack = [False] * len(self.nodes)
+        for node in range(len(self.nodes)):
+            if visited[node] == False:
+                if self.isCyclicUtil(node, visited, recStack, []) == True:
+                    return True
+        return False
+
+
+if __name__ == '__main__':
+
+    #Test graph with cycle
+    g1 = G(5)
+    g1.add_edge(0, 1, 1, False)
+    g1.add_edge(1, 2, 1, False)
+    g1.add_edge(2, 3, 1, False)
+    g1.add_edge(3, 4, 1, False)
+    g1.add_edge(4, 0, 1, False)
+
+    graph1 = Graph(5, g1, [0, 1, 2, 3, 4])
+
+    # Test graph without cycle
+    g2 = G(6)
+    g2.add_edge(0, 1, 1, False)
+    g2.add_edge(0, 2, 1, False)
+    g2.add_edge(1, 3, 1, False)
+    g2.add_edge(2, 3, 1, False)
+    g2.add_edge(3, 4, 1, False)
+    g2.add_edge(4, 5, 1, False)
+
+    graph2 = Graph(6, g2, [0, 1, 2, 3, 4, 5])
+
+    # Test graph with multiple cycles 
+    g3 = G(7)
+    g3.add_edge(0, 1, 1, False)
+    g3.add_edge(1, 2, 1, False)
+    g3.add_edge(2, 0, 1, False)
+    g3.add_edge(2, 3, 1, False)
+    g3.add_edge(3, 4, 1, False)
+    g3.add_edge(4, 2, 1, False)
+    g3.add_edge(5, 6, 1, False)
+    g3.add_edge(6, 5, 1, False)
+
+    graph3 = Graph(7, g3, [0, 1, 2, 3, 4, 5, 6])
+
+    if graph3.isCyclic():
+        print("Graph contains cycle")
+    else:
+        print("Graph doesn't contain cycle")
